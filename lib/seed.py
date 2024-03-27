1fac080125e39acd59e616b227f31b0e12f986a1
Teddy mwangi <teddy.maina@student.moringaschool.com>
2024-03-27 06:15:16 +0300

    completed assignment

--- lib/seed.py
+++ lib/seed.py
@@ -1,55 +1,31 @@
-#!/usr/bin/env python3
+# seed/seed.py
 
 from faker import Faker
-import random
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from models.models import Base, Game, Review
 
-from models import Game, Review
+fake = Faker()
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///one_to_many.db')
-    Session = sessionmaker(bind=engine)
-    session = Session()
+engine = create_engine('sqlite:///../one_to_many.db')
+Session = sessionmaker(bind=engine)
+session = Session()
 
-    session.query(Game).delete()
-    session.query(Review).delete()
-
-    fake = Faker()
-
-    genres = ['action', 'adventure', 'strategy',
-        'puzzle', 'first-person shooter', 'racing']
-    platforms = ['nintendo 64', 'gamecube', 'wii', 'wii u', 'switch',
-        'playstation', 'playstation 2', 'playstation 3', 'playstation 4',
-        'playstation 5', 'xbox', 'xbox 360', 'xbox one', 'pc']
-
-    games = []
-    for i in range(50):
-        game = Game(
-            title=fake.unique.name(),
-            genre=random.choice(genres),
-            platform=random.choice(platforms),
-            price=random.randint(5, 60)
-        )
-
-        # add and commit individually to get IDs back
-        session.add(game)
-        session.commit()
-
-        games.append(game)
-
-    reviews = []
-    for game in games:
-        for i in range(random.randint(1,5)):
-            review = Review(
-                score=random.randint(0, 10),
-                comment=fake.sentence(),
-                game_id=game.id
-            )
-
-            reviews.append(review)
-    
-    session.bulk_save_objects(reviews)
+# Generate fake data and populate the database
+for _ in range(10):
+    game = Game(
+        title=fake.name(),
+        genre=fake.random_element(elements=("Action", "Adventure", "Puzzle", "RPG")),
+        platform=fake.random_element(elements=("PC", "Xbox", "PlayStation", "Nintendo Switch")),
+        price=fake.random_int(min=10, max=60)
+    )
+    session.add(game)
     session.commit()
-    session.close()
+
+    review = Review(
+        score=fake.random_int(min=1, max=10),
+        comment=fake.text(),
+        game_id=game.id
+    )
+    session.add(review)
+    session.commit()
